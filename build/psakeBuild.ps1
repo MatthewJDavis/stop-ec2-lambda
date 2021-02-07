232b7ef582703bcb89fc5f021cda4f48aac179e4
MatthewJDavis <matthewdavis111@gmail.com>
2021-02-07 11:52:02 -0500

    update path

--- build/psakeBuild.ps1
+++ build/psakeBuild.ps1
@@ -1,9 +1,9 @@
 Task default -depends Analyse
 
-$scriptPath = '/Stop-EC2Instance/Stop-EC2Instance.ps1'
+$scriptPath = $PSScriptRoot + '/Stop-EC2Instance/Stop-EC2Instance.ps1'
 
 Task Analyse -description 'Analyse script with PSScriptAnalyzer' {
-  $saResults = Invoke-ScriptAnalyzer -Path "$PSScriptRoot$scriptPath" -Severity @('Error', 'Warning')
+  $saResults = Invoke-ScriptAnalyzer -Path $scriptPath -Severity @('Error', 'Warning')
   if ($saResults) {
     $saResults | Format-Table
     Write-Error -Message 'One or more Script Analyser errors/warnings were found'
